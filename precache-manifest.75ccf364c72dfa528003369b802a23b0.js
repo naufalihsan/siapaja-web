aaa5b80557eaa29d3ec5c119d707703327c85b0b
naufal ihsan <naufal.ihsan21@gmail.com>
2019-05-29 15:50:22 +0700

    Updates

---
+++ precache-manifest.75ccf364c72dfa528003369b802a23b0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/siapaja-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "7e0069c165a62037f3d8",
+    "url": "/siapaja-web/static/js/main.7e0069c1.chunk.js"
+  },
+  {
+    "revision": "131b7f1e91a652791f08f5ccfe702645",
+    "url": "/siapaja-web/static/media/line-awesome.131b7f1e.svg"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/siapaja-web/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/siapaja-web/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/siapaja-web/static/media/logo.a0185b04.svg"
+  },
+  {
+    "revision": "f79b1a95544cc2852059f87985bed3a9",
+    "url": "/siapaja-web/static/media/mecca.f79b1a95.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/siapaja-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/siapaja-web/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "50a7d07360c706b6785b",
+    "url": "/siapaja-web/static/js/runtime~main.50a7d073.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/siapaja-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/siapaja-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/siapaja-web/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/siapaja-web/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/siapaja-web/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "3c665e69e6470e5b6223",
+    "url": "/siapaja-web/static/js/2.3c665e69.chunk.js"
+  },
+  {
+    "revision": "3c665e69e6470e5b6223",
+    "url": "/siapaja-web/static/css/2.e05a4840.chunk.css"
+  },
+  {
+    "revision": "4aa145472b5d67cb8148d10533cdbdec",
+    "url": "/siapaja-web/index.html"
+  }
+];
